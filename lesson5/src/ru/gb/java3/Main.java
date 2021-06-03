f10e8a5d95455c3c3ebfdeaacf632b9487cb9507
obelix <sonicx921@yandex.ru>
2021-06-03 19:28:08 +0300

    Основное

--- lesson5/src/ru/gb/java3/Main.java
+++ lesson5/src/ru/gb/java3/Main.java
@@ -1,8 +1,37 @@
 package ru.gb.java3;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class Main {
 
+    public static final int CARS_COUNT = 4;
+
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        CyclicBarrier startLine = new CyclicBarrier(CARS_COUNT + 1);
+
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), startLine);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        try {
+            startLine.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            startLine.await();
+            startLine.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
     }
 }
